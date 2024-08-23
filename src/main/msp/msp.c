ca4605259bad5f7b42b558bad84b328856274bd5
Petri Mattila <petri.t.j.mattila@gmail.com>
2024-08-23 13:13:18 +0100

    🚁 Remove USE_ABSOLUTE_CONTROL

--- src/main/msp/msp.c
+++ src/main/msp/msp.c
@@ -1909,11 +1909,7 @@ case MSP_NAME:
         sbufWriteU8(dst, 0);
         sbufWriteU8(dst, 0);
 #endif
-#if defined(USE_ABSOLUTE_CONTROL)
-        sbufWriteU8(dst, currentPidProfile->abs_control_gain);
-#else
         sbufWriteU8(dst, 0);
-#endif
         sbufWriteU8(dst, 0);
 #if defined(USE_ACRO_TRAINER)
         sbufWriteU8(dst, currentPidProfile->acro_trainer_angle_limit);
@@ -3066,11 +3062,7 @@ static mspResult_e mspProcessInCommand(mspDescriptor_t srcDesc, int16_t cmdMSP, 
             sbufReadU8(src);
             sbufReadU8(src);
 #endif
-#if defined(USE_ABSOLUTE_CONTROL)
-            currentPidProfile->abs_control_gain = sbufReadU8(src);
-#else
             sbufReadU8(src);
-#endif
             sbufReadU8(src);
 #if defined(USE_ACRO_TRAINER)
             currentPidProfile->acro_trainer_angle_limit = sbufReadU8(src);
