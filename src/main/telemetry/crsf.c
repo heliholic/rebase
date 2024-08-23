1568fe469968aaa96441ab002d3e9a0b6ac7a9c7
Petri Mattila <petri.t.j.mattila@gmail.com>
2024-08-23 13:13:24 +0100

    🚁 Remove PASSTHRU_MODE

--- src/main/telemetry/crsf.c
+++ src/main/telemetry/crsf.c
@@ -455,8 +455,6 @@ static void crsfFrameFlightMode(sbuf_t *dst)
         flightMode = "!FS!";
     } else if (FLIGHT_MODE(GPS_RESCUE_MODE) || IS_RC_MODE_ACTIVE(BOXGPSRESCUE)) {
         flightMode = "RTH";
-    } else if (FLIGHT_MODE(PASSTHRU_MODE)) {
-        flightMode = "PASS";
     } else if (FLIGHT_MODE(ANGLE_MODE)) {
         flightMode = "ANGL";
     } else if (FLIGHT_MODE(POS_HOLD_MODE)) {
