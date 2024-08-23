385bfb1ec79c9678b2ac83eadcb0dace4c7d2347
Petri Mattila <petri.t.j.mattila@gmail.com>
2024-08-23 13:13:21 +0100

    🚁 Remove USE_GPS_LAP_TIMER

--- src/main/osd/osd.c
+++ src/main/osd/osd.c
@@ -63,7 +63,6 @@
 #include "drivers/pinio.h"
 
 #include "fc/core.h"
-#include "fc/gps_lap_timer.h"
 #include "fc/rc_controls.h"
 #include "fc/rc_modes.h"
 #include "fc/runtime_config.h"
@@ -578,19 +577,6 @@ void osdInit(displayPort_t *osdDisplayPortToUse, osdDisplayPortDevice_e displayP
     }
 }
 
-#ifdef USE_GPS_LAP_TIMER
-void printLapTime(char *buffer, const uint32_t timeMs) {
-    if (timeMs != 0) {
-        const uint32_t timeRoundMs = timeMs + 5; // round value in division by 10
-        const int timeSeconds = timeRoundMs / 1000;
-        const int timeDecimals = (timeRoundMs % 1000) / 10;
-        tfp_sprintf(buffer, "%3u.%02u", timeSeconds, timeDecimals);
-    } else {
-        tfp_sprintf(buffer, "  -.--");
-    }
-}
-#endif // USE_GPS_LAP_TIMER
-
 static void osdResetStats(void)
 {
     stats.max_current     = 0;
@@ -996,20 +982,6 @@ static bool osdDisplayStat(int statistic, uint8_t displayRow)
         osdDisplayStatisticLabel(midCol, displayRow, "MIN RSNR", buff);
         return true;
 #endif
-
-#ifdef USE_GPS_LAP_TIMER
-    case OSD_STAT_BEST_3_CONSEC_LAPS: {
-        printLapTime(buff, gpsLapTimerData.best3Consec);
-        osdDisplayStatisticLabel(midCol, displayRow, "BEST 3 CON", buff);
-        return true;
-    }
-
-    case OSD_STAT_BEST_LAP: {
-        printLapTime(buff, gpsLapTimerData.bestLapTime);
-        osdDisplayStatisticLabel(midCol, displayRow, "BEST LAP", buff);
-        return true;
-    }
-#endif // USE_GPS_LAP_TIMER
 
 #ifdef USE_PERSISTENT_STATS
     case OSD_STAT_TOTAL_FLIGHTS:
