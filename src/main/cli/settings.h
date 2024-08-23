84aab9f8f0252a816f850362cb01e727ba7e9ff0
Petri Mattila <petri.t.j.mattila@gmail.com>
2024-08-23 13:13:21 +0100

    🚁 Remove USE_SIMPLIFIED_TUNING

--- src/main/cli/settings.h
+++ src/main/cli/settings.h
@@ -117,7 +117,6 @@ typedef enum {
     TABLE_OSD_LOGO_ON_ARMING,
 #endif
     TABLE_MIXER_TYPE,
-    TABLE_SIMPLIFIED_TUNING_PIDS_MODE,
 #ifdef USE_OSD
     TABLE_CMS_BACKGROUND,
 #endif
@@ -246,8 +245,6 @@ extern const char * const lookupTableOsdDisplayPortDevice[];
 extern const char * const lookupTableFeedforwardAveraging[];
 
 extern const char * const lookupTableOffOn[];
-
-extern const char * const lookupTableSimplifiedTuningPidsMode[];
 
 extern const char * const lookupTableMixerType[];
 
