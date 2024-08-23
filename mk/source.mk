85fb8f75242392b8dfb54653113732a226fa0ebb
Petri Mattila <petri.t.j.mattila@gmail.com>
2024-08-23 13:13:20 +0100

    🚁 Remove USE_RPM_LIMIT

--- mk/source.mk
+++ mk/source.mk
@@ -216,7 +216,6 @@ COMMON_SRC = \
             cms/cms_menu_vtx_smartaudio.c \
             cms/cms_menu_vtx_tramp.c \
             cms/cms_menu_persistent_stats.c \
-            cms/cms_menu_rpm_limit.c \
             cms/cms_menu_quick.c \
             drivers/display_ug2864hsweg01.c \
             drivers/light_ws2811strip.c \
@@ -493,7 +492,6 @@ SIZE_OPTIMISED_SRC += \
             cms/cms_menu_vtx_smartaudio.c \
             cms/cms_menu_vtx_tramp.c \
             cms/cms_menu_persistent_stats.c \
-            cms/cms_menu_rpm_limit.c \
             cms/cms_menu_quick.c \
             io/vtx.c \
             io/vtx_rtc6705.c \
