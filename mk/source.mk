f89a4f1a0ac971da2a4961644309f4c99ed8ff84
Petri Mattila <petri.t.j.mattila@gmail.com>
2024-08-24 10:40:05 +0100

    🚁 Move failsafeConfig to pg/failsafe

--- mk/source.mk
+++ mk/source.mk
@@ -13,6 +13,7 @@ PG_SRC = \
             pg/bus_spi.c \
             pg/dashboard.c \
             pg/displayport_profiles.c \
+            pg/failsafe.c \
             pg/feature.c \
             pg/flash.c \
             pg/gps.c \
