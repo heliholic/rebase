54857341cf3c40e77634be39fb918e5a9f9bc52b
Petri Mattila <petri.mattila@unipart.com>
2025-01-03 23:23:04 +0000

    🚁 Remove USE_ITERM_RELAX

--- src/main/cli/settings.h
+++ src/main/cli/settings.h
@@ -97,10 +97,6 @@ typedef enum {
 #if defined(USE_VIDEO_SYSTEM)
     TABLE_VIDEO_SYSTEM,
 #endif
-#if defined(USE_ITERM_RELAX)
-    TABLE_ITERM_RELAX,
-    TABLE_ITERM_RELAX_TYPE,
-#endif
 #ifdef USE_ACRO_TRAINER
     TABLE_ACRO_TRAINER_DEBUG,
 #endif // USE_ACRO_TRAINER
@@ -250,10 +246,6 @@ extern const char * const lookupTableOpticalflowHardware[];
 extern const char * const lookupTableLedstripColors[];
 
 extern const char * const lookupTableRescueAltitudeMode[];
-
-extern const char * const lookupTableItermRelax[];
-
-extern const char * const lookupTableItermRelaxType[];
 
 extern const char * const lookupTableOsdDisplayPortDevice[];
 
