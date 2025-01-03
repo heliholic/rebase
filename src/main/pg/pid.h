8a6f1028745281dda7777fc595304715291b830a
Petri Mattila <petri.mattila@unipart.com>
2025-01-03 23:23:33 +0000

    🚁 Move pidConfig to pg/pid

---
+++ src/main/pg/pid.h
@@ -0,0 +1,57 @@
+/*
+ * This file is part of Rotorflight.
+ *
+ * Rotorflight is free software. You can redistribute this software
+ * and/or modify this software under the terms of the GNU General
+ * Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * Rotorflight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this software.
+ *
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <stdint.h>
+
+#include "pg/pg.h"
+
+enum {
+    PID_ROLL,
+    PID_PITCH,
+    PID_YAW,
+    PID_LEVEL,
+    PID_MAG,
+    PID_ITEM_COUNT
+};
+
+typedef struct {
+    uint8_t P;
+    uint8_t I;
+    uint8_t D;
+    uint16_t F;
+} pidf_t;
+
+#define MAX_PROFILE_NAME_LENGTH 8u
+
+typedef struct pidProfile_s {
+    char profileName[MAX_PROFILE_NAME_LENGTH + 1];
+    pidf_t  pid[PID_ITEM_COUNT];
+} pidProfile_t;
+
+PG_DECLARE_ARRAY(pidProfile_t, PID_PROFILE_COUNT, pidProfiles);
+
+typedef struct pidConfig_s {
+    uint8_t pid_process_denom;
+} pidConfig_t;
+
+PG_DECLARE(pidConfig_t, pidConfig);
