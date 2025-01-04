225ec9a240f6bba16a616f1c9f0da9e62ca0039f
Petri Mattila <petri.mattila@unipart.com>
2025-01-04 22:44:23 +0000

    🚁 Move opticalflowConfig to pg/opticalflow

--- src/main/pg/opticalflow.c
+++ src/main/pg/opticalflow.c
@@ -1,13 +1,13 @@
 /*
- * This file is part of Betaflight.
+ * This file is part of Rotorflight.
  *
- * Betaflight is free software. You can redistribute this software
+ * Rotorflight is free software. You can redistribute this software
  * and/or modify this software under the terms of the GNU General
  * Public License as published by the Free Software Foundation,
  * either version 3 of the License, or (at your option) any later
  * version.
  *
- * Betaflight is distributed in the hope that it will be useful,
+ * Rotorflight is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *
@@ -18,25 +18,24 @@
  *
  * If not, see <http://www.gnu.org/licenses/>.
  */
-#pragma once
 
-#include <stdint.h>
-#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 
-#include "drivers/opticalflow/opticalflow.h"
+#include "platform.h"
 
+#include "config/config_reset.h"
+
+#include "pg/pg.h"
+#include "pg/pg_ids.h"
 #include "pg/opticalflow.h"
 
-typedef struct opticalflow_s {
-    opticalflowDev_t dev;
-    int16_t quality;
-    vector2_t rawFlowRates;
-    vector2_t processedFlowRates;
-    uint32_t timeStampUs;
-} opticalflow_t;
+PG_REGISTER_WITH_RESET_TEMPLATE(opticalflowConfig_t, opticalflowConfig, PG_OPTICALFLOW_CONFIG, 0);
 
-bool opticalflowInit(void);
+PG_RESET_TEMPLATE(opticalflowConfig_t, opticalflowConfig,
+    .opticalflow_hardware = OPTICALFLOW_NONE,
+    .rotation = 0,
+    .flip_x = 0,
+    .flow_lpf = 0,
+);
 
-void opticalflowUpdate(void);
-bool isOpticalflowHealthy(void);
-void opticalflowProcess(void);
