a8aa8fce58eebe3025b8cb6bb2f01077986093dc
Petri Mattila <petri.mattila@unipart.com>
2025-01-06 16:01:57 +0000

    🚁 Move hardfaults.c to build/

--- mk/source.mk
+++ mk/source.mk
@@ -83,6 +83,7 @@ COMMON_SRC = \
             build/main.c \
             build/build_config.c \
             build/debug.c \
+            build/hardfaults.c \
             build/version.c \
             common/bitarray.c \
             common/colorconversion.c \
@@ -142,7 +143,6 @@ COMMON_SRC = \
             drivers/timer_common.c \
             fc/board_info.c \
             fc/dispatch.c \
-            fc/hardfaults.c \
             fc/tasks.c \
             fc/runtime_config.c \
             fc/stats.c \
