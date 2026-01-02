heliholic/devel

🚁 Rename _multirotor variants

src/main/pg/alt_hold.h
@@ -21,4 +21,14 @@
 
 #pragma once
 
-#include "pg/alt_hold_multirotor.h"
+#include <stdint.h>
+
+#include "pg/pg.h"
+
+typedef struct altHoldConfig_s {
+    uint8_t alt_hold_adjust_rate;
+    uint8_t alt_hold_deadband;
+} altHoldConfig_t;
+
+PG_DECLARE(altHoldConfig_t, altHoldConfig);
+

heliholic/rebase

src/main/pg/alt_hold.h
@@ -21,4 +21,14 @@
 
 #pragma once
 
-#include "pg/alt_hold_multirotor.h"
+#include <stdint.h>
+
+#include "pg/pg.h"
+
+typedef struct altHoldConfig_s {
+    uint8_t climbRate;
+    uint8_t deadband;
+} altHoldConfig_t;
+
+PG_DECLARE(altHoldConfig_t, altHoldConfig);
+