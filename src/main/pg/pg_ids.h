a8c8d9aa98f6c6ea637efc246fe82e8109af29d7
Petri Mattila <petri.t.j.mattila@gmail.com>
2026-01-02 21:38:53 +0000

    🚁 Remove mixers
    Remove BF mixer implementation and add stubs.

--- src/main/pg/pg_ids.h
+++ src/main/pg/pg_ids.h
@@ -24,7 +24,7 @@
 #define PG_FAILSAFE_CONFIG 1 // struct OK
 #define PG_BOARD_ALIGNMENT 2 // struct OK
 //#define PG_GIMBAL_CONFIG 3 // struct OK
-#define PG_MOTOR_MIXER 4 // two structs mixerConfig_t servoMixerConfig_t
+//#define PG_MOTOR_MIXER 4 // two structs mixerConfig_t servoMixerConfig_t
 #define PG_BLACKBOX_CONFIG 5 // struct OK
 #define PG_MOTOR_CONFIG 6 // struct OK
 #define PG_SENSOR_SELECTION_CONFIG 7 // struct OK
@@ -39,7 +39,7 @@
 #define PG_SYSTEM_CONFIG 18 // just has i2c_highspeed
 #define PG_FEATURE_CONFIG 19 // just has enabledFeatures
 #define PG_MIXER_CONFIG 20 // Cleanflight has single struct mixerConfig_t, betaflight has mixerConfig_t and servoMixerConfig_t
-#define PG_SERVO_MIXER 21 // Cleanflight has servoParam_t for each servo, betaflight has single servoParam_t
+//#define PG_SERVO_MIXER 21 // Cleanflight has servoParam_t for each servo, betaflight has single servoParam_t
 #define PG_IMU_CONFIG 22 // Cleanflight has imuConfig_t, betaflight has imuRuntimeConfig_t with additional parameters
 #define PG_PROFILE_SELECTION 23 // just contains current_profile_index
 #define PG_RX_CONFIG 24 // betaflight rxConfig_t contains different values
