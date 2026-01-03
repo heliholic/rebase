e1cf9dba3b54c3fcf84e1695ff0c5f3992d611bf
Petri Mattila <petri.t.j.mattila@gmail.com>
2026-01-03 22:18:14 +0000

    🚁 Refactor debug support

--- src/test/unit/althold_unittest.cc
+++ src/test/unit/althold_unittest.cc
@@ -104,7 +104,7 @@ TEST(AltholdUnittest, altHoldTransitionsTestUnfinishedExitEnter)
 
 extern "C" {
     uint8_t armingFlags = 0;
-    int16_t debug[DEBUG16_VALUE_COUNT];
+    int32_t debug[DEBUG_VALUE_COUNT];
     uint8_t debugMode;
     uint16_t flightModeFlags = 0;
     uint8_t stateFlags = 0;
