heliholic/devel

🚁 Rename USB product string to Rotorflight

src/platform/STM32/target/STM32F7X2/target.h
@@ -25,7 +25,7 @@
 #endif
 
 #ifndef USBD_PRODUCT_STRING
-#define USBD_PRODUCT_STRING     "Betaflight STM32F7x2"
+#define USBD_PRODUCT_STRING     "Rotorflight STM32F7x2"
 #endif
 
 #define USE_I2C_DEVICE_1

src/platform/STM32/target/STM32G47X/target.h
@@ -25,7 +25,7 @@
 #endif
 
 #ifndef USBD_PRODUCT_STRING
-#define USBD_PRODUCT_STRING     "Betaflight STM32G47x"
+#define USBD_PRODUCT_STRING     "Rotorflight STM32G47x"
 #endif
 
 #define USE_I2C_DEVICE_1

heliholic/rebase

src/platform/STM32/target/STM32F7X2/target.h
@@ -25,7 +25,7 @@
 #endif
 
 #ifndef USBD_PRODUCT_STRING
-#define USBD_PRODUCT_STRING     "Betaflight STM32F722"
+#define USBD_PRODUCT_STRING     "Rotorflight STM32F722"
 #endif
 
 #define USE_I2C_DEVICE_1

src/platform/STM32/target/STM32G47X/target.h
@@ -25,7 +25,7 @@
 #endif
 
 #ifndef USBD_PRODUCT_STRING
-#define USBD_PRODUCT_STRING     "Betaflight STM32G473"
+#define USBD_PRODUCT_STRING     "Rotorflight STM32G473"
 #endif
 
 #define USE_I2C_DEVICE_1