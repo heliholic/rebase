565ebb89c3ec7b2b6bd09fc3f6be68d7d8fff1ef
Petri Mattila <petri.t.j.mattila@gmail.com>
2026-08-15 20:22:51 +0100

    ⇆ Move hardfaults.c to build/

--- mk/source.mk
+++ mk/source.mk
@@ -47,6 +47,7 @@ PG_SRC = \
 COMMON_SRC = \
             build/build_config.c \
             build/debug.c \
+            build/faults.c \
             build/version.c \
             main.c \
             common/bitarray.c \
@@ -112,7 +113,6 @@ COMMON_SRC = \
             drivers/timer_common.c \
             fc/board_info.c \
             fc/dispatch.c \
-            fc/faults.c \
             fc/tasks.c \
             fc/runtime_config.c \
             fc/stats.c \
