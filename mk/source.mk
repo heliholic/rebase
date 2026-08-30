9a48bd2e9379802f62252b7e3bd173f69b46dd20
Petri Mattila <petri.t.j.mattila@gmail.com>
2026-08-30 12:12:39 +0100

    ⇆ Move gyroConfig to pg/gyro
    Extract the gyroConfig parameter group out of sensors/gyro into its own
    pg/gyro.[ch].

--- mk/source.mk
+++ mk/source.mk
@@ -24,6 +24,7 @@ PG_SRC = \
             pg/feature.c \
             pg/flash.c \
             pg/gps.c \
+            pg/gyro.c \
             pg/gyrodev.c \
             pg/imu.c \
             pg/max7456.c \
