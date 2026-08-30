82e57eee8cd23d25f5b9e0faafc4ce761c1e5f28
Petri Mattila <petri.t.j.mattila@gmail.com>
2026-08-30 12:10:31 +0100

    ⇆ Move servoConfig to pg/servo
    Extract the servoConfig, servoParams and servoDevConfig parameter groups
    out of flight/servos and drivers/servo_impl into pg/servo.[ch].

---
+++ src/main/pg/servo.h
@@ -0,0 +1,58 @@
+/*
+ * This file is part of Rotorflight.
+ *
+ * Rotorflight is free software. You can redistribute this software
+ * and/or modify this software under the terms of the GNU General
+ * Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * Rotorflight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this software.
+ *
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <stdint.h>
+
+#include "platform.h"
+
+#include "drivers/io_types.h"
+
+#include "pg/pg.h"
+
+#define DEFAULT_SERVO_MIN       1000
+#define DEFAULT_SERVO_MIDDLE    1500
+#define DEFAULT_SERVO_MAX       2000
+
+typedef struct {
+    uint32_t reversedSources;               // the direction of servo movement for each input source of the servo mixer, bit set=inverted
+    int16_t min;                            // servo min
+    int16_t max;                            // servo max
+    int16_t middle;                         // servo middle
+    int8_t rate;                            // range [-125;+125] ; can be used to adjust a rate 0-125% and a direction
+} servoParam_t;
+
+PG_DECLARE_ARRAY(servoParam_t, MAX_SUPPORTED_SERVOS, servoParams);
+
+typedef struct {
+    // PWM values, in milliseconds, common range is 1000-2000 (1ms to 2ms)
+    uint16_t servoCenterPulse;              // This is the value for servos when they should be in the middle. e.g. 1500.
+    uint16_t servoPwmRate;                  // The update rate of servo outputs (50-498Hz)
+    ioTag_t  ioTags[MAX_SUPPORTED_SERVOS];
+} servoDevConfig_t;
+
+typedef struct {
+    servoDevConfig_t dev;
+} servoConfig_t;
+
+PG_DECLARE(servoConfig_t, servoConfig);
+
