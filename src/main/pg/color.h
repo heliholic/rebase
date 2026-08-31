e1f0deb7a64d79ae071ca11e8fac8ba17ef8ae0d
Petri Mattila <petri.t.j.mattila@gmail.com>
2026-08-31 22:43:43 +0100

    ⇆ Add pg/color.h for hsvColor_t
    LED strip persisted colors no longer take this struct from common/color.h.

---
+++ src/main/pg/color.h
@@ -0,0 +1,30 @@
+/*
+ * This file is part of Rotorflight.
+ *
+ * Rotorflight is free software. You can redistribute this software
+ * and/or modify this software under the terms of the GNU General
+ * Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * Rotorflight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this software.
+ *
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <stdint.h>
+
+typedef struct hsvColor_s {
+    uint16_t h; // 0 - 359
+    uint8_t s;  // 0 - 255
+    uint8_t v;  // 0 - 255
+} hsvColor_t;
