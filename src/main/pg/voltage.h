7eecb774ad79d69d68905ef9ffdadb49d99c85c6
Petri Mattila <petri.t.j.mattila@gmail.com>
2026-09-01 11:22:56 +0100

    ⇆ Move voltageSensorADCConfig to pg/voltage
    Extract the voltageSensorADCConfig parameter group out of sensors/voltage
    into its own pg/voltage.[ch].

---
+++ src/main/pg/voltage.h
@@ -0,0 +1,50 @@
+/*
+ * This file is part of Rotorflight.
+ *
+ * Rotorflight is free software. You can redistribute this software
+ * and/or modify this software under the terms of the GNU General
+ * Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * Rotorflight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this software.
+ *
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <stdint.h>
+
+#include "pg/pg.h"
+
+#ifndef MAX_VOLTAGE_SENSOR_ADC
+#define MAX_VOLTAGE_SENSOR_ADC 1
+#endif
+
+#ifndef DEFAULT_VOLTAGE_METER_SCALE
+#define DEFAULT_VOLTAGE_METER_SCALE 110
+#endif
+
+#ifndef DEFAULT_VOLTAGE_METER_DIVIDER
+#define DEFAULT_VOLTAGE_METER_DIVIDER 10
+#endif
+
+#ifndef DEFAULT_VOLTAGE_METER_MULTIPLIER
+#define DEFAULT_VOLTAGE_METER_MULTIPLIER 1
+#endif
+
+typedef struct {
+    uint8_t vbatscale;                      // adjust this to match battery voltage to reported value
+    uint8_t vbatresdivval;                  // resistor divider R2 (default NAZE 10(K))
+    uint8_t vbatresdivmultiplier;           // multiplier for scale (e.g. 2.5:1 ratio with multiplier of 4 can use '100' instead of '25' in ratio) to get better precision
+} voltageSensorADCConfig_t;
+
+PG_DECLARE_ARRAY(voltageSensorADCConfig_t, MAX_VOLTAGE_SENSOR_ADC, voltageSensorADCConfig);
