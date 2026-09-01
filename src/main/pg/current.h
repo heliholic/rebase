ec8abbaeb5d911801a587056b30465d619019824
Petri Mattila <petri.t.j.mattila@gmail.com>
2026-09-01 11:22:16 +0100

    ⇆ Move currentSensorADCConfig to pg/current
    Extract the currentSensorADCConfig parameter group out of
    sensors/current into pg/current.[ch].

---
+++ src/main/pg/current.h
@@ -0,0 +1,34 @@
+/*
+ * This file is part of Rotorflight.
+ *
+ * Rotorflight is free software. You can redistribute this software
+ * and/or modify this software under the terms of the GNU General
+ * Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * Rotorflight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this software.
+ *
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <stdint.h>
+
+#include "pg/pg.h"
+
+typedef struct {
+    int16_t scale;              // scale the current sensor output voltage to milliamps. Value in mV/10A
+    int16_t offset;             // offset of the current sensor in mA
+} currentSensorADCConfig_t;
+
+PG_DECLARE(currentSensorADCConfig_t, currentSensorADCConfig);
+
