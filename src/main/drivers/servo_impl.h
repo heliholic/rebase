e4141242a4d9d83be5098bf15ee59ca4acb98f3a
Petri Mattila <petri.t.j.mattila@gmail.com>
2026-09-01 11:16:52 +0100

    ⇆ Move servoConfig to pg/servo
    Extract the servoConfig, servoParams and servoDevConfig parameter groups
    out of flight/servos and drivers/servo_impl into pg/servo.[ch]. Drop
    unused MAX_SERVO_SPEED, MAX_SERVO_BOXES and servoProfile_t.

--- src/main/drivers/servo_impl.h
+++ src/main/drivers/servo_impl.h
@@ -22,21 +22,11 @@
 #pragma once
 
 #include "platform.h"
-#include "drivers/io.h"
+
+#include "pg/servo.h"
 
 #define PWM_SERVO_MIN   500       // minimum servo PWM pulse width which we can set from cli
 #define PWM_SERVO_MAX   2500      // maximum servo PWM pulse width which we can set from cli
-
-#define DEFAULT_SERVO_MIN 1000
-#define DEFAULT_SERVO_MIDDLE 1500
-#define DEFAULT_SERVO_MAX 2000
-
-typedef struct servoDevConfig_s {
-    // PWM values, in milliseconds, common range is 1000-2000 (1ms to 2ms)
-    uint16_t servoCenterPulse;              // This is the value for servos when they should be in the middle. e.g. 1500.
-    uint16_t servoPwmRate;                  // The update rate of servo outputs (50-498Hz)
-    ioTag_t  ioTags[MAX_SUPPORTED_SERVOS];
-} servoDevConfig_t;
 
 void servoDevInit(const servoDevConfig_t *servoDevConfig);
 void servoWrite(uint8_t index, float value);
